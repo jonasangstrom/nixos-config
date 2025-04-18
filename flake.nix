4a841b34e43599a20a01f2b497a4702688789d58
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-04-18 16:55:33 +0200

    1st commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, ... } @ inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
