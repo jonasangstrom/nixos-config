be82bb72ce99f8bf4a5ab557033fd5a180ad3cca
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-04-23 21:58:32 +0200

    removed gnome

--- configuration.nix
+++ configuration.nix
@@ -63,7 +63,7 @@
 
       # Enable the GNOME Desktop Environment.
       displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
+      # desktopManager.gnome.enable = true;
 
       windowManager.awesome = {
       	enable = true;
