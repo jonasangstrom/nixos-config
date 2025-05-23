bf1b3b06a66f3eb4e175ab9cf225e46f1419f7a9
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-05-23 07:00:26 +0200

    Work on wezterm config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,9 +3,23 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+config.font = wezterm.font 'JetBrains Mono'
 config.default_prog = { 'fish', '-l' }
 config.color_scheme = 'Catppuccin Mocha'
 
 config.window_background_opacity = 0.85
+
+config.keys = {
+	-- This will create a new split and run the `top` program inside it
+	{
+		key = '%',
+		mods = 'CTRL|SHIFT|ALT',
+		action = wezterm.action.SplitPane {
+			direction = 'Left',
+			-- command = { args = { 'top' } },
+			size = { Percent = 50 },
+		},
+	},
+}
 
 return config
