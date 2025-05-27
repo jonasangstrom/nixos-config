321592918102d1d6228398c4212ce15c19b1a67a
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-05-27 22:53:10 +0200

    some basic wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,24 +3,49 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+
+local act = wezterm.action
+
 config.font = wezterm.font 'JetBrains Mono'
 config.default_prog = { 'fish', '-l' }
 config.color_scheme = 'Catppuccin Mocha'
 
 config.window_background_opacity = 0.85
+config.tab_and_split_indices_are_zero_based = true
 
 config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
-	-- This will create a new split and run the `top` program inside it
+	-- PANES
 	{
-		key = '%',
-		mods = 'CTRL|SHIFT|ALT',
-		action = wezterm.action.SplitPane {
-			direction = 'Left',
-			-- command = { args = { 'top' } },
-			size = { Percent = 50 },
-		},
+		key = 'c',
+		mods = 'LEADER',
+		action = act.SpawnTab("CurrentPaneDomain"),
 	},
+	{
+		key = 'n',
+		mods = 'LEADER',
+		action = act.ActivateTabRelative(1),
+	},
+	{
+		key = 'N',
+		mods = 'LEADER',
+		action = act.ActivateTabRelative(-1),
+	},
+	{ key = '0', mods = 'LEADER', action = act.ActivateTab(0) },
+	{ key = '1', mods = 'LEADER', action = act.ActivateTab(1) },
+	{ key = '2', mods = 'LEADER', action = act.ActivateTab(2) },
+	{ key = '3', mods = 'LEADER', action = act.ActivateTab(3) },
+	{ key = '5', mods = 'LEADER', action = act.ActivateTab(4) },
+	{ key = '6', mods = 'LEADER', action = act.ActivateTab(5) },
+	{ key = '7', mods = 'LEADER', action = act.ActivateTab(6) },
+	{ key = '8', mods = 'LEADER', action = act.ActivateTab(7) },
+	{ key = '9', mods = 'LEADER', action = act.ActivateTab(8) },
+
+	-- SPLITS
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 }
 
 return config
