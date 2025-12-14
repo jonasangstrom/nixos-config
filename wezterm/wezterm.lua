343a92310ecb7aba9f1c88ab99442056e866e410
Jonas Ångström <jonas.aangstroem@gmail.com>
2025-12-14 22:17:04 +0100

    panes numbers now correct

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,37 +15,38 @@ config.tab_and_split_indices_are_zero_based = true
 
 config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
-	-- PANES
-	{
-		key = 'c',
-		mods = 'LEADER',
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = 'n',
-		mods = 'LEADER',
-		action = act.ActivateTabRelative(1),
-	},
-	{
-		key = 'N',
-		mods = 'LEADER',
-		action = act.ActivateTabRelative(-1),
-	},
-	{ key = '0', mods = 'LEADER', action = act.ActivateTab(0) },
-	{ key = '1', mods = 'LEADER', action = act.ActivateTab(1) },
-	{ key = '2', mods = 'LEADER', action = act.ActivateTab(2) },
-	{ key = '3', mods = 'LEADER', action = act.ActivateTab(3) },
-	{ key = '5', mods = 'LEADER', action = act.ActivateTab(4) },
-	{ key = '6', mods = 'LEADER', action = act.ActivateTab(5) },
-	{ key = '7', mods = 'LEADER', action = act.ActivateTab(6) },
-	{ key = '8', mods = 'LEADER', action = act.ActivateTab(7) },
-	{ key = '9', mods = 'LEADER', action = act.ActivateTab(8) },
+    -- PANES
+    {
+        key = 'c',
+        mods = 'LEADER',
+        action = act.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+        key = 'n',
+        mods = 'LEADER',
+        action = act.ActivateTabRelative(1),
+    },
+    {
+        key = 'N',
+        mods = 'LEADER',
+        action = act.ActivateTabRelative(-1),
+    },
+    { key = '0', mods = 'LEADER', action = act.ActivateTab(0) },
+    { key = '1', mods = 'LEADER', action = act.ActivateTab(1) },
+    { key = '2', mods = 'LEADER', action = act.ActivateTab(2) },
+    { key = '3', mods = 'LEADER', action = act.ActivateTab(3) },
+    { key = '4', mods = 'LEADER', action = act.ActivateTab(4) },
+    { key = '5', mods = 'LEADER', action = act.ActivateTab(5) },
+    { key = '6', mods = 'LEADER', action = act.ActivateTab(6) },
+    { key = '7', mods = 'LEADER', action = act.ActivateTab(7) },
+    { key = '8', mods = 'LEADER', action = act.ActivateTab(8) },
+    { key = '9', mods = 'LEADER', action = act.ActivateTab(9) },
 
-	-- SPLITS
-	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+    -- SPLITS
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 }
 
 return config
